51bb6bb33dcdc028e8bffd1f3c935aa4fb940abc
jdamonew <41210780+jdamonew@users.noreply.github.com>
2019-11-26 12:37:40 -0300

    Ajustes de teste

--- indexTest.js
+++ indexTest.js
@@ -1,14 +1,13 @@
 var Table = function(){
-    this.id = [1,2,3,4,5,6,7]
+    this.id = [1,2,3,4,5,6,7,2000]
     this.name = ['Damone', 'Jóbia', 'Amanda', 'Maria', 'Filipe', 55];
-    this.old = [21,19,23,15,20,17];
+    this.old = [21,19,23,15,20,17,1.5];
 
 };
 
 Table.prototype.nome = function(){
 
     let nome = this.name[Math.floor(Math.random() * this.name.length)];
-    console.log('O nome é ' + nome);
     return nome;
 
 }
@@ -16,7 +15,6 @@ Table.prototype.nome = function(){
 Table.prototype.identidade = function(){
 
     let id = this.id[Math.floor(Math.random() * this.id.length)];
-    console.log('O ID é ' + id);
     return id;
 
 }
@@ -24,7 +22,6 @@ Table.prototype.identidade = function(){
 Table.prototype.idade = function(){
 
     let idade = this.old[Math.floor(Math.random() * this.old.length)];
-    console.log('A idade é ' + idade);
     return idade;
 
 }
--- test/recorde.js
+++ test/recorde.js
@@ -1,35 +1,58 @@
 var chai = require('chai').assert;
 var Recorde = require('../indexTest');
 
-describe('Teste de Funcionalidade', function(){
+describe('\t TESTE DE FUNCIONALIDADES ', function(){
 
     let baguio = Recorde;
     let lengthListID = baguio.id;
+    let lengthListIdade = baguio.old;
+    let lengthListNome = baguio.name;
 
-
-    it.skip('VERIFICA SE EXISTE UM ID IGUAL A 2', function(){
-        chai.equal(baguio.identidade(), 2);       
+    // 1
+    it.skip('VERIFICA SE EXISTE UM ID IGUAL A 2;', function(){
+        
+        for(let i=0; i < lengthListIdade.length; i++){
+            
+            chai.oneOf( 2 ,baguio.id);
+        }
     });
 
+    // 2
+    it.skip('VERIFICA SE O NOME RETORNA APENAS STRING;', function(){
+        //chai.isString(baguio.nome(), 'string');
 
-    it.skip('VERIFICA SE O NOME RETORNA UMA STRING', function(){
-        chai.isString(baguio.nome(), 'string');
+        for(let i=0; i < lengthListNome.length; i++){
+            
+            chai.isString(lengthListNome[i], 'string');
+        }
     });
 
-    it.skip('VERIFICA SE A IDADE É MAIOR QUE 18', function(){
-     chai.isAtLeast(baguio.idade(), 18);
+    // 3
+    it('VERIFICA SE A IDADE É MAIOR QUE 18;', function(){
+     chai.isAtLeast(baguio.idade(), 18, 'Idade menor que o permitido.');
     });
-    
-    it('VERIFICA SE EXISTE O ID INDICADO', function(){
 
+    // 4    
+    it.skip('VERIFICA SE EXISTE O ID INDICADO;', function(){
     
         for(let i=0; i < lengthListID.length; i++){
             
-            let id = baguio.identidade();
-            chai.oneOf( 7 ,baguio.id);
-
+            
+            chai.oneOf( 2000 ,baguio.id);
         }
- 
+    });
+
+    // 5
+    it.skip('VERIFICA SE EXISTE APENAS IDADE É INTEIRO;', function(){
+        
+        for(let i=0; i < lengthListIdade.length; i++){
+            
+            chai.isTrue(Number.isInteger(lengthListIdade[i]), 'Não é um numero inteiro: '  + lengthListIdade[i]);
+            
+        
+        }
+
+
     });
 
 });
